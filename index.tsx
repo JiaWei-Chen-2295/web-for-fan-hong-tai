617df60d7e69dcb162324477546ca5543b26d198
JiaWei-Chen-2295 <1601020332@qq.com>
2026-02-04 22:58:54 +0800

    refactor: Migrate font imports from HTML link tags to @fontsource packages.

--- index.tsx
+++ index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/ma-shan-zheng';
+import '@fontsource/noto-serif-sc';
+import '@fontsource/noto-serif-sc/700.css';
+import '@fontsource/zhi-mang-xing';
+import '@fontsource/be-vietnam-pro';
+import '@fontsource/be-vietnam-pro/500.css';
+import '@fontsource/be-vietnam-pro/700.css';
+import '@fontsource/plus-jakarta-sans';
+import '@fontsource/plus-jakarta-sans/500.css';
+import '@fontsource/plus-jakarta-sans/700.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
